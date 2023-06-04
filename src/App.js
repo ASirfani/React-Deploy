5c03566d447069b5f99d27b3fae1e9d8acf96001
Asirfani <abbasaliirfani@gmail.com>
2023-06-04 12:33:26 +0500

    commit is done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is for test
+          This is project is react-Live
         </p>
         <a
           className="App-link"
